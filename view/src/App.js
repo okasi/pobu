80eafdb3f0ad9b5ac4f087c458171df0aa424248
Esra Oktav Dinler <oktavesra@gmail.com>
2019-04-24 16:41:47 +0200

    Changed package.json to make it compatible with xcode

--- view/src/App.js
+++ view/src/App.js
@@ -10,103 +10,99 @@ import {
 } from 'react-native';
 import logo from './logo.png';
 
-// class App extends Component {
-//   state = {
-//     spinValue: new Animated.Value(0),
-//   }
+class App extends Component {
+  state = {
+    spinValue: new Animated.Value(0),
+  }
 
-//   onClick = () => {
-//     const wasRotated = this.state.spinValue._value === 1;
-//     Animated.timing(
-//       this.state.spinValue,
-//       {
-//         toValue: wasRotated ? 0 : 1,
-//         duration: 250,
-//         easing: Easing.linear
-//       }
-//     ).start()
-//   }
+  onClick = () => {
+    const wasRotated = this.state.spinValue._value === 1;
+    Animated.timing(
+      this.state.spinValue,
+      {
+        toValue: wasRotated ? 0 : 1,
+        duration: 250,
+        easing: Easing.linear
+      }
+    ).start()
+  }
 
-//   render() {
-//     const spin = this.state.spinValue.interpolate({
-//       inputRange: [0, 1],
-//       outputRange: ['0deg', '360deg']
-//     });
+  render() {
+    const spin = this.state.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
 
-//     return (
-//       <View style={styles.container}>
-//         <Animated.Image source={logo} style={[styles.logo, { transform: [{rotate: spin}] }]}/>
-//         <Text style={styles.title}>Create React Native Web App</Text>
-//         <Text>Open up src/App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         {Platform.OS !== 'web' && <Text>Shake your phone to open the developer menu.</Text>}
-//         <TouchableHighlight
-//           onPress={this.onClick}
-//           style={styles.button}
-//           underlayColor={'#0A84D0'}
-//         >
-//           <Text style={styles.buttonText}>Rotate Logo</Text>
-//         </TouchableHighlight>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   logo: {
-//     width: 300,
-//     height: 300,
-//   },
-//   title: {
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//   },
-//   button: {
-//     borderRadius: 3,
-//     padding: 20,
-//     marginVertical: 10,
-//     marginTop: 10,
-//     backgroundColor: '#1B95E0',
-//   },
-//   buttonText: {
-//     color: '#fff',
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//   },
-// });
-
-const App = () => {
-
-  const [users, setUsers] = useState([])
-
-
-  useEffect(() => {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => setUsers(users))
-
-  },[])
-
-  return (
-    <View>
-      <ul>
-        {users.map(user =>
-          <li key={user.id}>
-            {user.username}
-          </li>
-        )}
-      </ul>
-
-    </View>
-  )
-
+    return (
+      <View style={styles.container}>
+        <Animated.Image source={logo} style={[styles.logo, { transform: [{rotate: spin}] }]}/>
+        <Text style={styles.title}>POBUUUUU</Text>
+        <Text>Open up src/App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        {Platform.OS !== 'web' && <Text>Shake your phone to open the developer menu.</Text>}
+        <TouchableHighlight
+          onPress={this.onClick}
+          style={styles.button}
+          underlayColor={'#0A84D0'}
+        >
+          <Text style={styles.buttonText}>Rotate Logo</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  button: {
+    borderRadius: 3,
+    padding: 20,
+    marginVertical: 10,
+    marginTop: 10,
+    backgroundColor: '#1B95E0',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+// const App = () => {
+
+//   const [users, setUsers] = useState([])
+
+//   useEffect(() => {
+//     fetch('/users')
+//       .then(res => res.json())
+//       .then(users => setUsers(users))
+//   },[])
+
+//   return (
+//     <View>
+//       <ul>
+//         {users.map(user =>
+//           <li key={user.id}>
+//             {user.username}
+//           </li>
+//         )}
+//       </ul>
+//     </View>
+//   )
+// }
 
 let hotWrapper = () => () => App;
 if (Platform.OS === 'web') {
