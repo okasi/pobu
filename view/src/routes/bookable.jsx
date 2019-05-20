2469e121f6229a03223f19c0affe8bae2e9282f6
Esra Oktav Dinler <oktavesra@gmail.com>
2019-05-20 14:48:17 +0200

    Disabled the dates that have past+  Default fee is free when -> paid is true a string input is shown+ previous check boxes is now changing the state when checked and unchecked + Chat is checked and always true  + Added IRl - in real life meeting checkbox that is currently commented out where the user will get an input to specify the location+ the date and time now has the right formats

--- view/src/routes/bookable.jsx
+++ view/src/routes/bookable.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import moment, { now } from 'moment';
-
+import moment from 'moment';
 import DatePicker from 'react-datepicker';
 
 
@@ -10,14 +9,18 @@ export default function Bookable({ match }) {
   const [choosen, setChoosen] = useState();
   const [time, setTime] = useState("");
   const [type, setType] = useState("");
-  const [fee, setFee] = useState("");
-  const [video, setVideo] = useState("");
-  const [voice, setVoice] = useState("");
-  const [chat, setChat] = useState("");
+  const [fee, setFee] = useState(false);
+  const [video, setVideo] = useState(false);
+  const [voice, setVoice] = useState(false);
+  const [chat, setChat] = useState(true);
+  const [irl, setIrl] = useState(false);
+  const [location, setLocation] = useState("");
   const [theDuration, setTheDuration] = useState("");
 
   const [selDate, setSelDate] = useState("");
-  
+
+  const [nano, setNano] = useState("");
+
  
   return (
    
@@ -30,7 +33,8 @@ export default function Bookable({ match }) {
             Type:
             {/* <b>{type}</b> */}
           </label>
-          <input 
+          <input
+            style={{textTransform: 'capitalize'}} 
             type="text" 
             placeholder="Type of..."
             value={type}
@@ -46,14 +50,15 @@ export default function Bookable({ match }) {
           </label>
           <DatePicker
             selected={selDate}
-            onChange={input =>  setChoosen(''+  moment(input).format("dddd, MMMM Do YYYY"))}
-            // showTimeSelect
+            onChange={input =>  setChoosen(moment(input).format("dddd, MMMM Do YYYY"))}
             timeFormat="HH:mm"
             timeIntervals={15}
             dateFormat="MMMM d, yyyy h:mm aa"
-            timeCaption="time"
-            // placeholderText="Pick date & time"
             placeholderText={choosen}
+            minDate={moment().toDate()}
+            // timeCaption="time"
+            // showTimeSelect
+            // placeholderText="Pick date & time"
             // inline
           />
 
@@ -65,7 +70,7 @@ export default function Bookable({ match }) {
           </label>
           <DatePicker
             selected={selDate}
-            onChange={input =>  setTime(''+ moment(input).format("hh:mm"))}
+            onChange={input =>  setTime(moment(input).format("hh:mm a"))}
             showTimeSelect
             showTimeSelectOnly
             timeIntervals={15}
@@ -81,14 +86,14 @@ export default function Bookable({ match }) {
             {/* <b>{theDuration}</b> */}
           </label>
           <div className="form-input" name="duration">
-            <select onChange={e => setTheDuration(e.target.value)}>
+            <select onChange={e => setTheDuration(e.target.value+" min")}>
               <option value="" disabled selected>Duration:</option>
-              <option value="10 min">10 min</option>
-              <option value="20 min">20 min</option>
-              <option value="30 min">30 min</option>
-              <option value="40 min">40 min</option>
-              <option value="50 min">50 min</option>
-              <option value="60 min">60 min</option>
+              <option value="10">10 min</option>
+              <option value="20">20 min</option>
+              <option value="30">30 min</option>
+              <option value="40">40 min</option>
+              <option value="50">50 min</option>
+              <option value="60">60 min</option>
             </select>
             {/* <button>
               Add
@@ -106,21 +111,26 @@ export default function Bookable({ match }) {
               type="checkbox" 
               value={fee} 
               name="paid"
-              onClick={ () => setFee(' Paid')}
+              // onClick={ () => setFee(1)}
+              onClick={ () => setFee(!fee)}
             />
             <label for="paid" style={{margin: 'auto 10px auto 0'}}>
               Paid
             </label>
 
-            <input
-              type="checkbox"
-              value={fee} 
-              name="free"
-              onClick={ () => setFee(' Free')}
-            />
-            <label for="paid" style={{margin: 'auto 10px auto 0'}}>
-              free
-            </label>
+            {fee == true &&
+              <label style={{margin:'0', fontWeight: 'bold'}}>
+                Nano Wallet Public Key
+                <input 
+                type="text" 
+                name="nano"
+                onChange={e => setNano(e.target.value)}
+                maxlength="8"
+                />
+              </label>
+            }
+
+           
           </div>
 
             <br/>
@@ -134,7 +144,8 @@ export default function Bookable({ match }) {
               type="checkbox" 
               value={chat} 
               name="chat"
-              onClick={ () => setChat(' Chat')}
+              // onClick={ () => setChat(!chat)}
+              checked
             />
             <label for="text" style={{margin: 'auto 10px auto 0'}}>
               Chat
@@ -144,7 +155,7 @@ export default function Bookable({ match }) {
               type="checkbox" 
               value={voice} 
               name="voice"
-              onClick={ () => setVoice(' Voice')}
+              onClick={ () => setVoice(!voice)}
             />
             <label for="voice" style={{margin: 'auto 10px auto 0'}}>
               Voice
@@ -154,11 +165,12 @@ export default function Bookable({ match }) {
               type="checkbox"
               value={video}
               name="Video"
-              onClick={ () => setVideo(' Video')}
+              onClick={ () => setVideo(!video)}
             />
             <label for="Video" style={{margin: 'auto 10px auto 0'}}>
               Video
             </label>
+
 
             {/* <input type="checkbox" value="irl" name="IRL"/>
             <label for="irl" style={{margin: 'auto 10px auto 0'}}>
@@ -166,22 +178,83 @@ export default function Bookable({ match }) {
             </label> */}
           </div>
 
+          {/* <div className="form-input" name="fee">
+            <input 
+              type="checkbox" 
+              value={irl} 
+              name="irl"
+              // onClick={ () => setFee(1)}
+              onClick={ () => setIrl(!irl)}
+            />
+            <label for="paid" style={{margin: 'auto 10px auto 0'}}>
+              IRL
+            </label>
+
+            {irl == true &&
+              <label style={{margin:'0', fontWeight: 'bold'}}>
+                Location
+                <input 
+                type="text" 
+                name="location"
+                onChange={e => setLocation(e.target.value)}
+                />
+              </label>
+            }
+          </div> */}
+
           <div className="boxa"  style={{textAlign: 'right'}}>
             <span style={{ fontWeight: '500', textTransform:'capitalize'}}>{type}</span>
             <span style={{ fontWeight: '100', fontSize: '14px', margin: '5px 0', padding: '5px 0',borderBottom: ' 0.5px solid #d4d4d4'}}>
               {choosen}
               <br/>
               <span style={{fontSize: '16px'}}>{time}</span>
+              <br/>
+              
+              {/* {irl == true &&
+                <i style={{fontSize: '12px'}}>@{location}</i>
+              } */}
+            
             </span>
             <div style={{display: 'flex', flexDirection: 'row', justifyContent:'space-between', fontSize: '14px'}}>
               <i>{theDuration}</i>
-              <i>{fee}</i>
               <i>
-                {chat}
-                {voice}
-                {video}
+                {chat == true &&
+                  <i> Chat</i>
+                }
+                {voice == true && 
+                  <i>, Voice</i>
+                }
+                {video == true &&
+                  <i>, Video</i>
+                }
               </i>
-            </div>
+
+             
+
+              {fee == true &&
+                <i> Paid </i>
+              }
+              { fee == false &&
+                <i>Free </i>
+              }
+             
+             
+            </div> 
+            {/* Checks if wallet key is vaild by checking that it has 8 digits atm */}
+            {nano.length == 8 &&
+              <i style={{fontSize: '12px'}}>Payment Added</i>
+            }
+
+            {/* Checks if there is any input yet for the key*/}
+            {fee == true && nano.length == 0 &&
+              <i style={{fontSize: '12px'}}>Please add payment key</i>
+            }
+
+            {/* Checks if the key has an invaild count for the key*/}
+            {fee == true &&  nano.length >= 1 && nano.length < 8 && 
+              <i style={{fontSize: '12px'}}>Invaild key</i>
+            }
+           
           </div>
 
           <button style={{width: 150}} className="reg-btn">
@@ -189,19 +262,6 @@ export default function Bookable({ match }) {
           </button>
         </div>
 
-        {/* <div className="bookable-con-sub">
-          <h1>Recent Bookables</h1>
-          <div className="boxa">
-            <h5>Type of Service:{type}</h5>
-            <h5>Paid or free:{fee}</h5>
-            <h5>Chat:{chat}</h5>
-            <h5>Voice:{voice}</h5>
-            <h5>Video:{video}</h5>
-            <h5>Date:</h5>
-            <h5>Duration: {theDuration} min</h5>
-            <h5>Avaliable hours:</h5>
-          </div>
-        </div>     */}
       </div>
     </>
   );
