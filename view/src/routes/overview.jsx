210d2d1b0aabff7fc72aa82992a9ee7de6d6dbef
Esra Oktav Dinler <oktavesra@gmail.com>
2019-05-27 18:12:39 +0200

    :tada: The bookings + bookables now got their final styling, The bookings got labels on them to show the user if they are the host or client of a booking. +Responsiveness issues fixed where there where some major overflow issues on mobile devices

--- view/src/routes/overview.jsx
+++ view/src/routes/overview.jsx
@@ -20,7 +20,6 @@ const Overview = () => {
 
           const bookings = hBookings.concat(cBookings);
 
-          // sägTillDeBrur
           const populatedBookings = await Promise.all(
             await bookings.map(async booking => {
               if (booking._host) {
@@ -55,19 +54,18 @@ const Overview = () => {
           <h1>Bookings</h1>
           {/*Booked bookings as (client) */}
           {state.user && allBookings.length > 0 && allBookings.map(booking => {
-            if (booking._client == state.user._id) {
+            if (booking._client === state.user._id) {
               return (
                 <div className="overview-sub-card" key={booking._id}>
                   <div className="sub-card-top">
-                  <button onClick={(e) => {
-                    if (window.confirm(`Do you want to unbook ${booking.name} with ${booking.host.firstName}?`)) {
-                      actions({
-                        type: 'BOOKING_UNBOOK',
-                        payload: { bookableId: booking._id }
-                      })
-
-                    }
-                  }} className="deleteme" style={{ color: 'gray', background: 'none', border: 'none', padding: '0' }}>✖</button>
+                    <button onClick={(e) => {
+                      if (window.confirm(`Do you want to unbook ${booking.name} with ${booking.host.firstName}?`)) {
+                        actions({
+                          type: 'BOOKING_UNBOOK',
+                          payload: { bookableId: booking._id }
+                        })
+                      }
+                    }} className="deleteme">✖</button>
                     <span>
                       {booking.name}
                       <span>
@@ -77,24 +75,25 @@ const Overview = () => {
                     </span>
                   </div>
                   <div className="sub-card-top">
-                    <span className="sub-card-date">
-                      <br />
-                      <span>
-                       Client
+                    <span>
+                      <br/>
+                      <span className="client">
+                        Client
                       </span>
                     </span>
                     <span className="sub-card-date">
+                      <span className="date">
+                        {moment(booking.date).format('MM/DD/YYYY')}
+                      </span>
                       <br />
                       <span>
-                        {moment(booking.date).format('MM/DD/YYYY hh:mm')}
+                        {moment(booking.date).format('hh:mm')}
                       </span>
                     </span>
                   </div>
                   <div className="sub-card-details">
                     <i>{booking.duration} min</i>
-                    <i>
-                      {booking.communication}
-                    </i>
+                    <i>{booking.communication}</i>
                     {booking.fee === 1 &&
                       <i> Paid </i>
                     }
@@ -103,7 +102,7 @@ const Overview = () => {
                     }
                   </div>
                   <i className="sub-card-urlbox">
-                    <a href={`/booking/${booking._id}`}>
+                    <a href={`/booking/${booking._id}`} className="asclient-url">
                       {booking._id}
                     </a>
                   </i>
@@ -120,43 +119,41 @@ const Overview = () => {
                   <div className="sub-card-top">
                     <button onClick={(e) => {
                       if (window.confirm(`Do you want to delete ${booking.name} with ${booking.client.firstName}?`)) {
-
                         alert('Deleted')
                         actions({
                           type: 'BOOKING_DELETE',
                           payload: { bookableId: booking._id }
                         })
-
                       }
-                    }} className="deleteme" style={{ color: 'gray', background: 'none', border: 'none', padding: '0' }}>✖</button>
+                    }} className="deleteme">✖</button>
                     <span>
                       {booking.name}
                       <span>
                         with
                       </span>
-                      {/* {booking._host} */}
-                      {/* booking._client.slice(-10) */}
                       {booking.client.firstName}
                     </span>
                   </div>
                   <div className="sub-card-top">
-                  <span className="sub-card-date">
-                  <br />
-                    host
-                  </span>
+                    <span >
+                      <br />
+                      <span className="host">
+                        host
+                      </span>
+                    </span>
                     <span className="sub-card-date">
-                      {moment(booking.date).format('MM/DD/YYYY')}
+                      <span className="date">
+                        {moment(booking.date).format('MM/DD/YYYY')}
+                      </span>
                       <br />
                       <span>
                         {moment(booking.date).format('hh:mm')}
                       </span>
-                  </span>
+                    </span>
                   </div>
                   <div className="sub-card-details">
                     <i>{booking.duration} min</i>
-                    <i>
-                      {booking.communication}
-                    </i>
+                    <i>{booking.communication}</i>
                     {booking.fee === 1 &&
                       <i> Paid </i>
                     }
@@ -165,7 +162,7 @@ const Overview = () => {
                     }
                   </div>
                   <i className="sub-card-urlbox">
-                    <a href={`/booking/${booking._id}`}>
+                    <a href={`/booking/${booking._id}`} className="ashost-url">
                       {booking._id}
                     </a>
                   </i>
@@ -176,66 +173,49 @@ const Overview = () => {
         </div>
 
 
+        {/*Bookables as (Host) */}
         <div className="overview-card">
-          <span style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <span className="bookablescolumn">
             <h1>Your Bookables</h1>
             <NavLink to="/bookable">
               <h1>+</h1>
             </NavLink>
           </span>
 
-          {/*Bookables as (Host) */}
           {state.user && allBookings && allBookings.map((booking) => {
-      
             if (booking._host === state.user._id && !booking._client) {
-        
               return (
                 <div className="overview-sub-card" key={booking._id}>
                   <div className="sub-card-top">
                     <button onClick={(e) => {
-                      
                       if (window.confirm(`Do you want to delete ${booking.name}?`)) {
-
                         alert('Deleted')
                         actions({
                           type: 'BOOKING_DELETE',
                           payload: { bookableId: booking._id }
                         })
-
                       }
-                    }} className="deleteme" style={{ color: 'gray', background: 'none', border: 'none', padding: '0' }}>✖</button>
+                    }} className="deleteme">✖</button>
                     <span>
                       {booking.name}
                       <span>
                         with
                       </span>
-                      {/* {booking._host} */}
-                      {/* {booking._client &&
-                      <i>HE</i>
-                      } */}
                       {booking.host && booking.host.firstName}
                       (You)
                     </span>
                   </div>
-                  <div className="sub-card-top">
                   <span className="sub-card-date">
-                  {booking._client &&
-                      <i style={{color: 'red'}}>booked</i> 
-                      }
-                      </span>
-                  <span className="sub-card-date">
-                    {moment(booking.date).format('MM/DD/YYYY')}
+                      {moment(booking.date).format('MM/DD/YYYY')}
                     <br />
                     <span>
                       {moment(booking.date).format('hh:mm')}
                     </span>
                   </span>
-                  </div>
+             
                   <div className="sub-card-details">
                     <i>{booking.duration} min</i>
-                    <i>
-                      {booking.communication}
-                    </i>
+                    <i>{booking.communication}</i>
                     {booking.fee === 1 &&
                       <i> Paid </i>
                     }
@@ -243,16 +223,14 @@ const Overview = () => {
                       <i>Free </i>
                     }
                   </div>
-
                   {!booking._client &&
                   <i className="sub-card-urlbox">
-                    <a href={`/booking/${booking._id}`}>
+                    <a href={`/booking/${booking._id}`} className="asbookable-url">
                       {booking._id}
                     </a>
                   </i>
                   }
-                </div>
-                
+                </div> 
               )
             }
           })}
